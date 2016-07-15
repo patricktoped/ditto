f9600057524515fd380cba312fb97164a466a10b
Patrick Alexander <patrick.alexander@tokopedia.com>
2016-07-15 18:15:36 +0700

    add reset buyer and seller notification

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,7 @@
 var client = ZAFClient.init();
 var config;
 var user_id;
+var shop_id;
 
 client.invoke('resize', { width: '100%', height: '250px' });
 client.metadata().then(function(metadata) {
@@ -13,8 +14,19 @@ $(function() {
     });  
     
     $(document).on("click", "#go-to-home", function(){
-        $("#top-nav").removeClass("hidden");
+        renderNavigation({
+            'user_id' : user_id,
+            'shop_id' : shop_id
+        });
         renderPage('home');
+    }); 
+    
+    $(document).on("click", "#reset-buyer-notif", function(){
+        resetNotification({ user_id : user_id }, 'buyer');
+    }); 
+    
+    $(document).on("click", "#reset-seller-notif", function(){
+        resetNotification({ shop_id : shop_id },'seller');
     }); 
 });
 
@@ -38,12 +50,67 @@ function requestUserInfo() {
     }); 
 }
 
+function resetNotification(dt, type){
+    $.ajax({
+        dataType: "jsonp",
+        type: "GET",
+        data : dt,
+        url: config.subdomain+"/ajax/zendesk/zendesk.pl",
+        jsonpCallback: "data_handler",
+        timeout: 5000,
+        success: function(result){
+            var notif_data = new Object();
+            
+            if(result.success){
+                notif_data['type'] = type;
+                
+                if(type == "buyer"){}
+                    notif_data['buyer'] = 1;
+                    notif_data['notification'] = {
+                        'buyer_order_status'  : result.notification.buyer_order_status,
+                        'buyer_order_deliver' : result.notification.buyer_order_deliver,
+                        'buyer_order_cancel'  : result.notification.buyer_order_cancel,
+                        'buyer_pay_confirm'   : result.notification.buyer_pay_confirm,
+                        'buyer_pay_confirmed' : result.notification.buyer_pay_confirmed
+                    };
+                }else if(type == "seller"){
+                    notif_data['notification'] = {
+                        'seller_new_order'     : result.notification.seller_new_order,
+                        'seller_shipping_conf' : result.notification.seller_shipping_conf,
+                        'seller_deliver_order' : result.notification.seller_deliver_order
+                    };
+                }
+                renderPage('resetSuccess', notif_data);
+            }
+            else{
+                renderPage('error');
+            }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            renderPage('error', XMLHttpRequest);
+        },
+        beforeSend: function(){
+            showLoading();
+        },
+        complete: function(){
+            console.log("complete");
+        }
+    });
+}
+
+function renderNavigation(data){
+    var html = Handlebars.compile($("#navbar-template").html())(data);
+    $("#top-nav").removeClass("hidden");
+    $("#top-nav").html(html);
+}
+
 function renderPage(name, data){
     var templates = {
-        'index'     : '#index-template',
-        'home'      : '#home-template',
-        'loading'   : '#loading-template',
-        'error'     : '#error-template'
+        'index'        : '#index-template',
+        'home'         : '#home-template',
+        'loading'      : '#loading-template',
+        'error'        : '#error-template',
+        'resetSuccess' : '#reset-template'
     };
 
     var container = $("#main-content");
@@ -59,10 +126,7 @@ function renderPage(name, data){
         navbar.click();
     };
     
-    if($("#apps-container").hasClass("loading")){
-        $("#apps-container").removeClass("loading");
-    }
-    
+    hideLoading();
 }
 
 function formatDate(date) {
@@ -76,11 +140,21 @@ function formatDate(date) {
     return date;
 }
 
+function showLoading(){
+    $("#apps-container").addClass("loading");
+}
+
+function hideLoading(){
+    if($("#apps-container").hasClass("loading")){
+        $("#apps-container").removeClass("loading");
+    }
+}
+
 function showUserInfo(data) {
     var dt = new Object();
     dt.action     = "get_user_info";
-    dt.user_email = data.user.email;
-    
+    dt.user_email = encodeURIComponent(data.user.email);
+
     $.ajax({
         dataType: "jsonp",
         type: "GET",
@@ -89,28 +163,37 @@ function showUserInfo(data) {
         jsonpCallback: "data_handler",
         timeout: 5000,
         success: function(result){
-            var requester_data = {
-                'name'    : data.user.name,
-                'email'   : data.user.email,
-                'status'  : result.status,
-                'user_id' : result.user_id
-            };
-            user_id = result.user_id;
+            var requester_data = new Object();
+            
+            if(result.user){
+                user_id = result.user.id;
+                requester_data['user'] = {
+                    'id'     : result.user.id,
+                    'name'   : data.user.name,
+                    'email'  : data.user.email,
+                    'status' : result.user.status,
+                    'url'    : config.subdomain+"/people/"+result.user.id
+                };
+                if(result.shop){
+                    shop_id = result.shop.id;
+                    requester_data['shop'] = {
+                        'id'     : result.shop.id,
+                        'name'   : result.shop.name,
+                        'domain' : result.shop.domain,
+                        'url'    : config.subdomain+"/"+result.shop.domain
+                    };
+                }
+            }
+
             renderPage('index', requester_data);
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
-            // tokopedia.alert_error();
-            console.log(XMLHttpRequest);
-            console.log(textStatus);
-            console.log(errorThrown);
             renderPage('error', XMLHttpRequest);
         },
         beforeSend: function(){
-            // Handle the beforeSend event
-            console.log("before send");
+            showLoading();
         },
         complete: function(){
-            // Handle the complete event
             console.log("complete");
         }
     });
